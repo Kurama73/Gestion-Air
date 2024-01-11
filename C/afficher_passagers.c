4dd0a7140670e5b2084fc3e7cfd93cb2a0eb322f
FRUMY TOM p2300641 <tom.frumy@etu.univ-lyon1.fr>
2024-01-11 13:38:00 +0000

    Update afficher_passagers.c

--- C/afficher_passagers.c
+++ C/afficher_passagers.c
@@ -1,6 +1,6 @@
 /**
  * \file     afficher_passagers.c
- * \brief
+ * \brief    ensemble des fonctions pour afficher le tableau des passagers d'un embarquement 
  * \author   Tom FRUMY et Enzo SACCONE
  * \version  0.1
  * \date     19/12/23
@@ -19,11 +19,9 @@
 */
 void afficherPassagersMoins12ans(struct Vol tableau_vols[], int numero_vol)
 {
-
-
     for (int i = 0; i < tableau_vols[numero_vol].nombre_passagers; i++)
     {
-        // Vérifier si le passager a moins de 12 ans
+        // Vrifier si le passager a moins de 12 ans
         if (estMoins12ans(tableau_vols[numero_vol].passagers[i]))
         {
             printf("| %-18s | %-18s | %02d/%02d/%4d        | %-5d | %-11.2f |\n",
@@ -40,7 +38,7 @@ void afficherPassagersMoins12ans(struct Vol tableau_vols[], int numero_vol)
 
 /**
 * \fn  afficherPassagersPlus12ans(struct Vol tableau_vols[], int numero_vol)
-* \brief Affiche le tableau des passagers de moins de 12 ans
+* \brief Affiche le tableau des passagers de plus de 12 ans
 *
 * \param tableau_vols est un tableau et nombre_vols est un entier
 * \return rien
@@ -52,7 +50,7 @@ void afficherPassagersPlus12ans(struct Vol tableau_vols[], int numero_vol)
 
     for (int i = 0; i < tableau_vols[numero_vol].nombre_passagers; i++)
     {
-        // Vérifier si le passager a moins de 12 ans
+        // Vrifier si le passager a moins de 12 ans
         if (estPlus12ans(tableau_vols[numero_vol].passagers[i]))
         {
             printf("| %-18s | %-18s | %02d/%02d/%4d        | %-5d | %-11.2f |\n",
@@ -69,25 +67,24 @@ void afficherPassagersPlus12ans(struct Vol tableau_vols[], int numero_vol)
 
 /**
 * \fn  estMoins12ans(struct Passager passager)
-* \brief Fonction pour vérifier si un passager a moins de 12 ans
+* \brief Fonction pour vÃ©rifier si un passager a moins de 12 ans
 *
 * \param passager est un tableau
 * \return entier
 * \post age < 12 si l'age est plus petit que 12
 */
-
 int estMoins12ans(struct Passager passager)
 {
-    // Supposons que nous sommes en 2024 (vous pouvez ajuster cela en fonction de l'année actuelle)
+    // Supposons que nous sommes en 2024 (vous pouvez ajuster cela en fonction de l'anne actuelle)
     int age = 2024 - passager.annee_naissance;
 
-    // Vérifier si le passager a moins de 12 ans
+    // Vrifier si le passager a moins de 12 ans
     return age < 12;
 }
 
 /**
 * \fn  estPLus12ans(struct Passager passager)
-* \brief Fonction pour vérifier si un passager a plus de 12 ans
+* \brief Fonction pour vÃ©rifier si un passager a plus de 12 ans
 *
 * \param passager est un tableau
 * \return entier
@@ -95,16 +92,16 @@ int estMoins12ans(struct Passager passager)
 */
 int estPlus12ans(struct Passager passager)
 {
-    // Supposons que nous sommes en 2024 (vous pouvez ajuster cela en fonction de l'année actuelle)
+    // Supposons que nous sommes en 2024 (vous pouvez ajuster cela en fonction de l'anne actuelle)
     int age = 2024 - passager.annee_naissance;
 
-    // Vérifier si le passager a moins de 12 ans
+    // Vrifier si le passager a moins de 12 ans
     return age >= 12;
 }
 
 /**
 * \fn  afficherPassagers_entete(int numero_vol)
-* \brief Affiche un entête pour une meilleur visibilité
+* \brief Affiche un entÃªte pour une meilleur visibilitÃ©
 *
 * \param  numero_vols est un entier
 * \return rien
